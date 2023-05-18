d6913a3468858f69eb646c0863520a4df1fa2d92
himangsu roy <himur98@gmail.com>
2023-05-18 22:03:47 +0600

    error page some modified

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -19,12 +19,11 @@ const Error = () => {
               Page not found
             </h1>
             <p className="mt-4 text-gray-500 dark:text-gray-400">
-              Sorry, the page you are looking for doesn't exist.Here are some
-              helpful links:
+              Sorry, the page you are looking for doesn't exist.
             </p>
 
             <div className="flex items-center mt-6 gap-x-3">
-              <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+              <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700 cursor-pointer">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -44,7 +43,7 @@ const Error = () => {
               </button>
 
               <Link to="/">
-                <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600 cursor-pointer">
                   Take me home
                 </button>
               </Link>
