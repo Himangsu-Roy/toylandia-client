4dd081cca85957781a1b7545c0326645cbd7e41c
himangsu roy <himur98@gmail.com>
2023-05-18 20:24:43 +0600

    error page added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Error = () => {
   const navigate = useNavigate();
